cfc62888d270fbfd02ea0b8711fc355b248286f4
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-02-12 23:44:35 +0200

    Added storehouse class

--- src/PizzaProject.java
+++ src/PizzaProject.java
@@ -1,4 +1,31 @@
 public class PizzaProject {
+    public class Storehouse {
+        int stored_dough, stored_cheese, stored_sauce, stored_pepperoni, stored_sausage, stored_drink;
+    
+        public Storehouse() {
+            this.stored_dough = 0;
+            this.stored_sauce = 0;
+            this.stored_cheese = 0;
+            this.stored_pepperoni = 0;
+            this.stored_sausage = 0;
+            this.stored_drink = 0;
+        }
+    
+        public void storage_display () {
+            int[] storage = this.storage_return();
+            String[] ingredients = {"Dough", "Sauce", "Cheese", "Pepperoni", "Sausage", "Drink"};
+            System.out.println("Storage data:");
+            for (int i = 0; i < storage.length; i++) {
+                System.out.println("    " + (i + 1) + ") " + ingredients[i] + ": " + storage[i] + ".");
+            }
+        }
+    
+        public int[] storage_return () {
+            int[] result = {stored_dough, stored_sauce, stored_cheese, stored_pepperoni, stored_sausage, stored_drink};
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("I love you, PizzaProject <3");
     }
