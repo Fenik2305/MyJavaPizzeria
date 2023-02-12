e7f83f7e85b5d8b311e4a235397aea1b8abfe7ff
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-02-12 14:20:39 +0200

    Rude commit

--- src/PizzaProject.java
+++ src/PizzaProject.java
@@ -1,5 +1,5 @@
 public class PizzaProject {
     public static void main(String[] args) {
-        System.out.println("Hello, PizzaProject!");
+        System.out.println("Goodbye, PizzaProject :(");
     }
 }
