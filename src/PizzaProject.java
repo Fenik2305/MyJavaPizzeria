b68a57ad2003299e2db08ea3b92b912bddbe1485
Mykola_Fedorchenko <fenik2305@gmail.com>
2023-02-12 23:27:13 +0200

    Lovely commit

--- src/PizzaProject.java
+++ src/PizzaProject.java
@@ -1,5 +1,5 @@
 public class PizzaProject {
     public static void main(String[] args) {
-        System.out.println("Goodbye, PizzaProject :(");
+        System.out.println("I love you, PizzaProject <3");
     }
 }
